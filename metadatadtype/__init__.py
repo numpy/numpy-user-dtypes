26849e8f6fd8602738f787a55dbaaacfcd808c81
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2022-11-15 15:10:56 -0700

    make flake8 happy

--- metadatadtype/__init__.py
+++ metadatadtype/__init__.py
@@ -1,2 +1,8 @@
+"""A dtype that carries around metadata
+
+This is an example usage of the experimental new dtype API
+in Numpy and is not intended for any real purpose.
+"""
+
 from ._metadatadtype_main import MetadataDType
 from .scalar import MetadataScalar
