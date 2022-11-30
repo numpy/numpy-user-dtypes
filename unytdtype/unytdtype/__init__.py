297a8fa4d220f331ecce2f304ad1a8a50d183206
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2022-11-30 11:46:26 -0700

    fix unytdtype tests

--- unytdtype/unytdtype/__init__.py
+++ unytdtype/unytdtype/__init__.py
@@ -4,7 +4,7 @@ This is an example usage of the experimental new dtype API
 in Numpy and is not yet intended for any real purpose.
 """
 
+from .scalar import UnytScalar  # isort: skip
 from ._unytdtype_main import UnytDType
-from .scalar import UnytScalar
 
 __all__ = ["UnytDType", "UnytScalar"]
