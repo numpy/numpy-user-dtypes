66db4c01bda4f53ac438c395b47f374d414584ff
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2022-12-07 15:05:04 -0700

    add tests for truncation behavior at creation time

--- asciidtype/tests/test_asciidtype.py
+++ asciidtype/tests/test_asciidtype.py
@@ -10,11 +10,40 @@ def test_dtype_creation():
 
 def test_scalar_creation():
     dtype = ASCIIDType(7)
-    ASCIIScalar('string', dtype)
+    ASCIIScalar("string", dtype)
 
 
 def test_creation_with_explicit_dtype():
     dtype = ASCIIDType(7)
     arr = np.array(["hello", "this", "is", "an", "array"], dtype=dtype)
     assert repr(arr) == (
-        "array(['hello', 'this', 'is', 'an', 'array'], dtype=ASCIIDType(7))")
+        "array(['hello', 'this', 'is', 'an', 'array'], dtype=ASCIIDType(7))"
+    )
+
+
+def test_truncation():
+    inp = ["hello", "this", "is", "an", "array"]
+
+    dtype = ASCIIDType(5)
+    arr = np.array(inp, dtype=dtype)
+    assert repr(arr) == (
+        "array(['hello', 'this', 'is', 'an', 'array'], dtype=ASCIIDType(5))"
+    )
+
+    dtype = ASCIIDType(4)
+    arr = np.array(inp, dtype=dtype)
+    assert repr(arr) == (
+        "array(['hell', 'this', 'is', 'an', 'arra'], dtype=ASCIIDType(4))"
+    )
+
+    dtype = ASCIIDType(1)
+    arr = np.array(inp, dtype=dtype)
+    assert repr(arr) == (
+        "array(['h', 't', 'i', 'a', 'a'], dtype=ASCIIDType(1))"
+    )
+    assert arr.tobytes() == b"h\x00t\x00i\x00a\x00a\x00"
+
+    dtype = ASCIIDType()
+    arr = np.array(["hello", "this", "is", "an", "array"], dtype=dtype)
+    assert repr(arr) == ("array(['', '', '', '', ''], dtype=ASCIIDType(0))")
+    assert arr.tobytes() == b"\x00\x00\x00\x00\x00"
