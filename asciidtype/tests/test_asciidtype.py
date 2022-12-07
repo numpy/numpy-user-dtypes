f20e8e1d6c5f2eb5a1ccc0372d87f9329ffcd8b0
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2022-12-07 15:51:42 -0700

    add casting tests

--- asciidtype/tests/test_asciidtype.py
+++ asciidtype/tests/test_asciidtype.py
@@ -21,7 +21,7 @@ def test_creation_with_explicit_dtype():
     )
 
 
-def test_truncation():
+def test_creation_truncation():
     inp = ["hello", "this", "is", "an", "array"]
 
     dtype = ASCIIDType(5)
@@ -47,3 +47,27 @@ def test_truncation():
     arr = np.array(["hello", "this", "is", "an", "array"], dtype=dtype)
     assert repr(arr) == ("array(['', '', '', '', ''], dtype=ASCIIDType(0))")
     assert arr.tobytes() == b"\x00\x00\x00\x00\x00"
+
+
+def test_casting_to_asciidtype():
+    arr = np.array(["hello", "this", "is", "an", "array"], dtype=ASCIIDType(5))
+
+    assert repr(arr.astype(ASCIIDType(7))) == (
+        "array(['hello', 'this', 'is', 'an', 'array'], dtype=ASCIIDType(7))"
+    )
+
+    assert repr(arr.astype(ASCIIDType(5))) == (
+        "array(['hello', 'this', 'is', 'an', 'array'], dtype=ASCIIDType(5))"
+    )
+
+    assert repr(arr.astype(ASCIIDType(4))) == (
+        "array(['hell', 'this', 'is', 'an', 'arra'], dtype=ASCIIDType(4))"
+    )
+
+    assert repr(arr.astype(ASCIIDType(1))) == (
+        "array(['h', 't', 'i', 'a', 'a'], dtype=ASCIIDType(1))"
+    )
+
+    assert repr(arr.astype(ASCIIDType())) == (
+        "array(['', '', '', '', ''], dtype=ASCIIDType(0))"
+    )
