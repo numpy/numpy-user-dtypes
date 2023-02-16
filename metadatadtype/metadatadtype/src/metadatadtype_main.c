901de15aa9eecb49600de1ba8a481be8737e9284
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2023-02-16 12:06:05 -0700

    increment experimental API version import for all dtypes

--- metadatadtype/metadatadtype/src/metadatadtype_main.c
+++ metadatadtype/metadatadtype/src/metadatadtype_main.c
@@ -21,7 +21,7 @@ PyInit__metadatadtype_main(void)
     if (_import_array() < 0) {
         return NULL;
     }
-    if (import_experimental_dtype_api(6) < 0) {
+    if (import_experimental_dtype_api(7) < 0) {
         return NULL;
     }
 
