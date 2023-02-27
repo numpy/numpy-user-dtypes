67f3bbbc073eca88c48ce3a1659325d1f795b0a4
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2023-02-27 09:28:48 -0700

    update experimental API version in all dtypes

--- mpfdtype/mpfdtype/src/mpfdtype_main.c
+++ mpfdtype/mpfdtype/src/mpfdtype_main.c
@@ -22,7 +22,7 @@ PyInit__mpfdtype_main(void)
     if (_import_array() < 0) {
         return NULL;
     }
-    if (import_experimental_dtype_api(8) < 0) {
+    if (import_experimental_dtype_api(9) < 0) {
         return NULL;
     }
 
