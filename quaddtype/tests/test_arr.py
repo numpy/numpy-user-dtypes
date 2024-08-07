f53c1308367f0f011dea7af32f4bc7603879d0da
swayaminsync <hawkempire007@gmail.com>
2024-08-08 02:02:07 +0530

    fixing test namings

--- quaddtype/tests/test_arr.py
+++ quaddtype/tests/test_arr.py
@@ -4,6 +4,6 @@ import numpy as np
 from quaddtype import QuadPrecDType, QuadPrecision
 
 
-def test():
+def test_dtype():
     a = QuadPrecision("1.63")
     assert f"{np.array([a], dtype=QuadPrecDType).dtype}" == "QuadPrecDType()"
