fe476c2ea755651be1e203df2557c9a2715ef27c
Juniper Tyree <50025784+juntyr@users.noreply.github.com>
2025-08-23 04:04:00 +0000

    Use int64_t for sleef precision

--- quaddtype/numpy_quaddtype/src/quaddtype_main.c
+++ quaddtype/numpy_quaddtype/src/quaddtype_main.c
@@ -80,12 +80,12 @@ get_sleef_constant(PyObject *self, PyObject *args)
     else if (strcmp(constant_name, "precision") == 0) {
         Py_DECREF(result);
         // precision = int(-log10(epsilon))
-        auto precision = Sleef_cast_to_int64q1(Sleef_negq1(Sleef_log10q1_u10(SLEEF_QUAD_EPSILON)));
+        int64_t precision = Sleef_cast_to_int64q1(Sleef_negq1(Sleef_log10q1_u10(SLEEF_QUAD_EPSILON)));
         return PyLong_FromLong(precision);
     }
     else if (strcmp(constant_name, "resolution") == 0) {
         // precision = int(-log10(epsilon))
-        auto precision = Sleef_cast_to_int64q1(Sleef_negq1(Sleef_log10q1_u10(SLEEF_QUAD_EPSILON)));
+        int64_t precision = Sleef_cast_to_int64q1(Sleef_negq1(Sleef_log10q1_u10(SLEEF_QUAD_EPSILON)));
         // resolution = 10 ** (-precision)
         result->value.sleef_value = Sleef_powq1_u10(Sleef_cast_from_int64q1(10), Sleef_cast_from_int64q1(-precision));
     }
