84b29111bc86abbab9d3fe3d59c05a9845412e20
SwayamInSync <hawkempire007@gmail.com>
2025-09-06 08:10:36 +0000

    some utilities

---
+++ quaddtype/sdist_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+
+if [ -d "build/" ]; then
+    rm -rf dist/
+fi
+
+python -m pip uninstall -y numpy_quaddtype
+python -m build --sdist --outdir dist/
+python -m pip install dist/numpy_quaddtype-0.1.0.tar.gz -v --no-build-isolation -Cbuilddir=build
