57fdfe0c2abd23b38de7b4b529aa55b3daf57c2b
jorenham <jhammudoglu@gmail.com>
2025-09-17 22:47:27 +0200

    TYP: Static typing support for `numpy_quaddtype`

---
+++ quaddtype/numpy_quaddtype/__init__.pyi
@@ -0,0 +1,56 @@
+from typing import Final
+
+from ._quaddtype_main import (
+    QuadPrecDType,
+    QuadPrecision,
+    _IntoQuad,  # type-check only  # pyright: ignore[reportPrivateUsage]
+    get_num_threads,
+    get_quadblas_version,
+    is_longdouble_128,
+    set_num_threads,
+)
+
+__all__ = [
+    "QuadPrecision",
+    "QuadPrecDType",
+    "SleefQuadPrecision",
+    "LongDoubleQuadPrecision",
+    "SleefQuadPrecDType",
+    "LongDoubleQuadPrecDType",
+    "is_longdouble_128",
+    "pi",
+    "e",
+    "log2e",
+    "log10e",
+    "ln2",
+    "ln10",
+    "max_value",
+    "epsilon",
+    "smallest_normal",
+    "smallest_subnormal",
+    "bits",
+    "precision",
+    "resolution",
+    "set_num_threads",
+    "get_num_threads",
+    "get_quadblas_version",
+]
+
+def SleefQuadPrecision(value: _IntoQuad) -> QuadPrecision: ...
+def LongDoubleQuadPrecision(value: _IntoQuad) -> QuadPrecision: ...
+def SleefQuadPrecDType() -> QuadPrecDType: ...
+def LongDoubleQuadPrecDType() -> QuadPrecDType: ...
+
+pi: Final[QuadPrecision] = ...
+e: Final[QuadPrecision] = ...
+log2e: Final[QuadPrecision] = ...
+log10e: Final[QuadPrecision] = ...
+ln2: Final[QuadPrecision] = ...
+ln10: Final[QuadPrecision] = ...
+max_value: Final[QuadPrecision] = ...
+epsilon: Final[QuadPrecision] = ...
+smallest_normal: Final[QuadPrecision] = ...
+smallest_subnormal: Final[QuadPrecision] = ...
+resolution: Final[QuadPrecision] = ...
+bits: Final = 128
+precision: Final = 33
