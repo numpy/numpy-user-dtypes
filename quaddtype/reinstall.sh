dde614b204aef9d100e34e7871220f1e952ded6f
SwayamInSync <hawkempire007@gmail.com>
2025-12-06 14:38:30 +0000

    enable SSE

--- quaddtype/reinstall.sh
+++ quaddtype/reinstall.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -x
 
-if [ -d "build/" ]; then
-    rm -r build
-    rm -rf dist/
-    rm -rf subprojects/qblas
-    rm -rf subprojects/sleef
-fi
+# Always clean build artifacts and subprojects
+rm -rf build/
+rm -rf dist/
+rm -rf subprojects/qblas
+rm -rf subprojects/sleef
+rm -rf .mesonpy-*
 
 python -m pip uninstall -y numpy_quaddtype
 python -m pip install . -vv --no-build-isolation 2>&1 | tee build_log.txt
